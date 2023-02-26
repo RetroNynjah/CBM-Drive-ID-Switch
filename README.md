9e6514f0160bfdc478f87152d8ab7bbfb915c166
retroninja <59034596+RetroNynjah@users.noreply.github.com>
2023-02-26 22:34:28 +0100

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# CBM-Drive-ID-Switch
-Software controlled ID switch for Commodore disk drives.
+# CBM Drive ID Switch
+<img src="images/rev1/pcb_top.png" alt="PCB rendering" width="400"/> <img src="images/rev1/pcb_bottom.png" alt="PCB rendering" width="400"/><br/>
+
+This is a software controlled device ID switch for Commodore disk drives. It can be installed in any Commodore IEC disk drive but it only makes sense to install it in the drives that lack external DIP switches for ID selection but handy for the 1541 drive and for computers with built-in disk drives such as SX-64, C128D and C128DCR. 
+
+## How it works
+There's a microcontroller on the PCB that listens for strings of text on the drive data bus. Once a predefined string of bytes arrives on the bus, it flips the drive ID select pins on the VIA and resets the drive. 
+The command that the drive listens for is x@RNDRVID where x is a number representing the drive ID to switch to.
+Valid device numbers are 8, 9, 10, 11. In the switch command, these are specified as 8, 9, 0, 1. I.e. 0@RNDRVID is the command for switching to device #10. See the usage section for more information on the command.
+
+
+## Installation
+
+The installation is quite simple if the VIA chips are socketed. Just remove the VIA chip indicated in the table below, the insert the switch and install the VIA chip in the switch.  
+If the VIA is soldered to the PCB you will need to first desolder the VIA and install a socket (unless you want to solder the switch to the PCB which I wouldn't recommend).
+
+|Model			| VIA			| 
+|:-----------:	|:-----------:	|
+|1541			| UC3 or UAB1	|
+|1571			| U9		  	|
+|128DCR			| U106			|
+|SX-64\*		| UBC3			|
+
+\* Not tested yet
+
+<img src="images/rev1/1541installed.jpg" alt="Switch installed in CBM 1541" width="600"/>
+
+## Usage
+
+The device ID can be switched from basic using regular CBM DOS commands or by using DOS wedge commands from JiffyDOS or similar. 
+Once the ID has been switched, the drive will keep the new ID for subsequent startups until the ID is changed again. Resetting or power cycling the drive doesn't affect the ID.
+
+In the below examples we switch ID of device #8 to device #9. 
+
+#### From CBM DOS
+
+The easiest command to use to get bytes on the drive bus is the LOAD command but other disk commands can be used too.  
+```LOAD "9@RNDRVID",8```  
+This generates a file not found error (unless you have a file named 9@RNDRVID ;) ). This error is to be expected and nothing to worry about.  
+
+#### From JiffyDOS
+```@"9@RNDRVID",8```  
+or activate the drive first using CTRL+D or using @8 and use this command:  
+```@9@RNDRVID```
+
+
+## BOM
+
+
+|Qty|Identifier |Part						|Comments							|
+|---|----------	|---------------------------|-----------------------------------|
+| 1 |IC2        |ATmega48A (TQFP32)			|Or compatible						|
+| 1 |IC4        |SN74AHCT273NSR (SO20)     	|									|
+| 1 |IC5        |SN74AHCT1G04DBVR (SOT23-5)	|									|
+| 3 |C1,C2,C5   |100nF 0805    				|Ceramic capacitor					|
+| 2 |C3,C4      |30pF 0805					|Ceramic capacitor					|
+| 1 |R1         |10kΩ 0805					|									|
+| 1 |Y1         |ECS-200-20-3X-TR			|Crystal 20MHz						|
+| 2 |			|Female header row 			|20pin 2.54mm (for VIA chip)        |
+| 2 |			|Male header row			|20pin 2.54mm (round or flat pins)	|
+| 1 |D1			|LED 0805					|Optional							|
+| 1 |R2			|560Ω 0805					|Optional							|
+
+You can use any larger pin-compatible ATmega MCU such as ATmega88 or ATmega328. All variants seem to work including the not 100% pin-compatible PB version. 
+The test points TP1 and TP2 are connected to ATmega pins PC1 and PC2 (Arduino A1 and A2) and can possibly be used for debugging or for some external connections. 
+The optional LED (D1) and its resistor can also be used for debugging purposes but are difficult to see when VIA is installed so the may as well be left out. 
+
+<img src="images/rev1/build.jpg" alt="Completed switch" width="600"/>
---
+++ firmware/CBM_drive_ID_switch_fw_v1_0/CBM_drive_ID_switch_fw_v1_0.ino
@@ -0,0 +1,181 @@
+/*
+#########################################################################################
+#                                                                                       #
+#  ID switch sketch for CBM disk drives                                                 #
+#  To be used with the Retroninja CBM Drive ID Switch in all CBM IEC drives             #
+#                                                                                       #
+#  Version 1.0                                                                          #
+#  https://github.com/retronynjah                                                       #
+#                                                                                       #
+#########################################################################################
+*/
+
+#include <EEPROM.h>
+
+// searchString is the command that is used for switching ROM.
+// It is passed by the 1541-II in reverse order so the variable should be reversed too.
+// The command should be preceded by a ROM number between 1 and 4 when used.
+// The below reversed searchString in hex ascii is MORNR@ which is specified like this on the C64: 1@RNROM, 2@RNROM and so on.
+//byte searchString[] = {0x4D,0x4F,0x52,0x4E,0x52,0x40};
+//1@RNDRVID
+//DIVRDNR@
+byte searchString[] = {0x44,0x49,0x56,0x52,0x44,0x4E,0x52,0x40};
+
+// pin defitions
+#define RESETPIN 10
+#define CLOCKPIN 13
+#define LEDPIN A0
+#define LOWBIT 8
+#define HIGHBIT 9
+
+int commandLength = sizeof(searchString);
+int bytesCorrect = 0;
+volatile bool state;
+static byte byteCurr;
+
+void pciSetup(byte pin)
+{
+    *digitalPinToPCMSK(pin) |= bit (digitalPinToPCMSKbit(pin));  // enable pin
+    PCIFR  |= bit (digitalPinToPCICRbit(pin)); // clear any outstanding interrupt
+    PCICR  |= bit (digitalPinToPCICRbit(pin)); // enable interrupt for the group
+}
+
+
+
+ISR (PCINT0_vect) // handle pin change interrupt for D8 to D13 here
+{    
+       // read state of pin 13
+       state = PINB & B00100000;
+}
+ 
+
+void cleareeprom(){
+  for (int i = 0 ; i < EEPROM.length() ; i++) {
+    EEPROM.write(i, 0);
+  }
+}
+
+
+void resetdrive(){
+
+  digitalWrite(RESETPIN, LOW);
+  pinMode(RESETPIN, OUTPUT);
+  delay(50);
+  digitalWrite(RESETPIN, HIGH);
+  pinMode(RESETPIN, INPUT);
+
+}
+
+
+void switchid(byte deviceid){
+  /*
+  for (int x = 0; x <= deviceid; x++){
+    digitalWrite (LEDPIN, HIGH);
+    delay(30);
+    digitalWrite (LEDPIN, LOW);
+    delay(250);
+  }
+  delay (200);
+  */  
+  switch (deviceid){
+    case 8:
+      // device 8
+      digitalWrite(LOWBIT , LOW);
+      digitalWrite(HIGHBIT , LOW);
+      break;
+    case 9:
+      // device 9
+      digitalWrite(LOWBIT , HIGH);
+      digitalWrite(HIGHBIT , LOW);
+      break;
+    case 0:
+      // device 10
+      digitalWrite(LOWBIT , LOW);
+      digitalWrite(HIGHBIT , HIGH);
+      break;
+    case 1:
+      // device 11
+      digitalWrite(LOWBIT , HIGH);
+      digitalWrite(HIGHBIT , HIGH);
+      break;
+    default:
+      break;      
+  }
+
+  byte savedid = EEPROM.read(0);
+  if (savedid != deviceid){
+    EEPROM.write(0, deviceid);
+  }
+  resetdrive();
+}
+
+
+void setup() {
+
+  pinMode(LOWBIT, OUTPUT); // device ID low bit (6522 pin 15)
+  pinMode(HIGHBIT, OUTPUT); // device ID high bit (6522 pin 16)
+  digitalWrite(LOWBIT,HIGH);
+  digitalWrite(HIGHBIT,HIGH);
+  pinMode(CLOCKPIN, INPUT); // R/!W
+  pinMode(RESETPIN, INPUT); // Keep reset pin as input while not performing reset.
+
+  pinMode (LEDPIN, OUTPUT);
+
+  // set data pins as inputs
+  DDRD = B00000000;
+
+  // retrieve last used device ID from ATmega EEPROM and switch ID using CIA pin 15/16
+  int lastid = EEPROM.read(0);
+  if (lastid > 9){
+    cleareeprom();
+    lastid = 8;
+  }
+  switchid(lastid);
+  
+  // enable pin change interrupt on pin D13(PB5) connected to R/W on 6522
+  pciSetup(CLOCKPIN); 
+  
+}
+
+
+void loop() {
+  
+  if (state == HIGH){
+    byteCurr = PIND;
+    state=LOW;
+      
+    // we don't have full search string yet. check if current byte is what we are looking for
+    if (byteCurr == searchString[bytesCorrect]){
+      // It is the byte we're waiting for. increase byte counter
+      bytesCorrect++;
+	    if (bytesCorrect == commandLength){
+        // we have our full search string. wait for next byte
+		    while(state == LOW){}
+    		byteCurr = PIND;
+    		// This byte should be the id number
+    		// valid numbers are 1-4 (ASCII 49-52)
+    		if ((byteCurr >= 48)&&(byteCurr<=57)){
+              // rom number within valid range. Switch rom
+              switchid(byteCurr - 48);
+    		}
+    		else if(byteCurr == searchString[0]){
+              // it was the first byte in string, starting with new string
+              bytesCorrect = 1;
+    		}
+        else{
+          bytesCorrect = 0;
+        }
+  	  }
+    }
+    // byte isn't what we are looking for, is it the first byte in the string then?
+    else if(byteCurr == searchString[0]){
+        // it was the first byte in string, starting with new string
+        bytesCorrect = 1;
+    }
+    else {
+      // byte not correct at all. Start over from the beginning
+      bytesCorrect = 0;
+    }
+  }
+  
+}
